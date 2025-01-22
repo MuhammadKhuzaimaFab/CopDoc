776414788f7323cfaa93051c6065bc9f2e672f76
MuhammadKhuzaimaFab <muhammad.khuzaima@fabtechsol.com>
2025-01-21 17:52:07 -0800

    add 4 more pages

--- dashboard js/dashboard.js
+++ dashboard js/dashboard.js
@@ -70,8 +70,45 @@ function again_cut() {
 //       }
 //     }
 //   });
-  const ctx = document.getElementById('myPieChart').getContext('2d');
-  const myPieChart = new Chart(ctx, {
+ 
+  const ctx2 = document.getElementById('mySecondPieChart').getContext('2d');
+  const myPieChart2 = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: 'Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        }
+      }
+    }
+  });
+  const ctx3 = document.getElementById('mythirdPieChart').getContext('2d');
+  const myPieChart3 = new Chart(ctx3, {
     type: 'pie',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
